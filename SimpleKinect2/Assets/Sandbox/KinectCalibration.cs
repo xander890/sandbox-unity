c9a9e303285de4e828b0c25f75eaf3be603ddd18
Alessandro Dal Corso <alcor@dtu.dk>
2018-05-06 15:27:58 +0200

    calibration reading scripts.

---
+++ SimpleKinect2/Assets/Sandbox/KinectCalibration.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Linq;
+using System;
+using System.Xml.Linq;
+using System.Drawing;
+
+
+public class KinectCalibration : MonoBehaviour {
+
+    public Vector2Int projectorResolution;
+    public Vector2Int kinectResolution;
+    public Matrix4x4 projectionMatrix;
+
+
+    Vector2Int parseVector2Int(string s)
+    {
+        string[] ss = s.Split(',');
+        Vector2Int res = new Vector2Int();
+        res.Set(Int32.Parse(ss[0]), Int32.Parse(ss[1]));
+        return res;
+    }
+
+
+    void loadCalibration()
+    {
+        string xml_data = Application.dataPath + "/StreamingAssets/settings/calibration.xml";
+        XDocument doc = XDocument.Load(xml_data);
+        string projectorResolutionS = (string)doc.Element("CALIBRATION").Element("RESOLUTIONS").Element("PROJECTOR");
+        projectorResolution = parseVector2Int(projectorResolutionS);
+        string kinectResolutionS = (string)doc.Element("CALIBRATION").Element("RESOLUTIONS").Element("KINECT");
+        kinectResolution = parseVector2Int(kinectResolutionS);
+        var ecoefficients = doc.Element("CALIBRATION").Element("COEFFICIENTS");
+        projectionMatrix = Matrix4x4.identity;
+        for (int i = 0; i < 11; i++)
+        {
+            string coeff_string = "COEFF" + i;
+            float value = System.Single.Parse((string)ecoefficients.Element(coeff_string));
+            int row = i / 4;
+            int column = i % 4;
+            projectionMatrix[row, column] = value;
+        }
+        projectionMatrix[2, 3] = 1.0f;
+    }
+
+	// Use this for initialization
+	void Start () {
+        loadCalibration();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SimpleKinect2/Assets/Sandbox/KinectSettings.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Linq;
+using System;
+using System.Xml.Linq;
+using System.Drawing;
+
+public class KinectSettings : MonoBehaviour
+{
+    public Rectangle kinectROI;
+    public Vector3 basePlaneNormal;
+    public Vector3 basePlaneOffset;
+    public float maxOffsetBack;
+    public int averagingSlots;
+    public bool followBigChanges;
+    public bool outlierInpainting;
+    public bool fullFrameFiltering;
+    public bool spatialFiltering;
+
+
+    Vector3 parseVector3(string s)
+    {
+        string[] ss = s.Split(',');
+        Vector3 res = new Vector3();
+        res.Set(float.Parse(ss[0]), float.Parse(ss[1]), float.Parse(ss[2]));
+        return res;
+    }
+
+    void loadKinectProjector()
+    {
+        string xml_data = Application.dataPath + "/StreamingAssets/settings/kinectProjectorSettings.xml";
+        XDocument doc = XDocument.Load(xml_data);
+        var root = doc.Element("KINECTSETTINGS");
+        kinectROI = new Rectangle();
+        string[] roi = ((string)root.Element("kinectROI")).Split(',');
+        kinectROI.X = Int32.Parse(roi[0]);
+        kinectROI.Y = Int32.Parse(roi[1]);
+        kinectROI.Width = Int32.Parse(roi[3]);
+        kinectROI.Height = Int32.Parse(roi[4]);
+        basePlaneNormal = parseVector3((string)root.Element("basePlaneNormalBack"));
+        basePlaneOffset = parseVector3((string)root.Element("basePlaneOffsetBack"));
+        maxOffsetBack = float.Parse((string)root.Element("maxOffsetBack"));
+        averagingSlots = Int32.Parse((string)root.Element("numAveragingSlots"));
+        followBigChanges = ((string)root.Element("followBigChanges")) == "0" ? false : true;
+        outlierInpainting = ((string)root.Element("OutlierInpainting")) == "0" ? false : true;
+        fullFrameFiltering = ((string)root.Element("FullFrameFiltering")) == "0" ? false : true;
+        spatialFiltering = ((string)root.Element("spatialFiltering")) == "0" ? false : true;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        loadKinectProjector();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
