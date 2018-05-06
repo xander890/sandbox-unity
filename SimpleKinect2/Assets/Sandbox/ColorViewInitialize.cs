9b8ea3f7688c05de32a56064be1244eadc0cb94c
Andrea Luongo <aluo@win.dtu.dk>
2018-05-06 15:52:32 +0200

    "new scene, new UI element with depth texture and initialize script"

---
+++ SimpleKinect2/Assets/Sandbox/ColorViewInitialize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorViewInitialize : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition3D = new Vector3(0, 0, 0);
+        rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+        rectTransform.localScale = new Vector3(1, 1, 1);
+        //rectTransform
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
